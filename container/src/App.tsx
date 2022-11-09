f93b57b71e134128081a502047c46f761a21876b
Mritunjay ZL <mritunjay.saha@ziroh.com>
2022-11-09 11:16:37 +0530

    refactor: angular application

--- container/src/App.tsx
+++ container/src/App.tsx
@@ -7,14 +7,14 @@ import {
   Image,
   Link,
   Text,
+  Spacer,
 } from "@chakra-ui/react";
 import React from "react";
 
-import LeftSideBar from "./modules/LeftSideBar";
-
 const CounterAppOne = React.lazy(() => import("app1/CounterAppOne"));
 const CounterAppTwo = React.lazy(() => import("app2/CounterAppTwo"));
-// const LeftSideBar = React.lazy(() => import("leftSideBar/leftSideBar"));
+const LeftSideBar = React.lazy(() => import("./modules/LeftSideBar"));
+
 const version = "";
 
 const App = () => (
@@ -27,21 +27,19 @@ const App = () => (
       p="0"
       flexDirection="column"
     >
-      <Box color="#fff" position="fixed" right="0" top="0" mr="2rem" mt="2rem">
-        Latest Build Date: <Text fontWeight="bold">{version}</Text>
-      </Box>
       <Flex
         border="1px solid #151421"
         borderRadius="1rem"
-        height="50vh"
-        justifyContent="space-around"
+        height="max-content"
+        justifyContent="space-between"
         alignItems="center"
         flexDirection="column"
         padding="5rem"
         backgroundColor="#6F60EA"
       >
-        <Heading color="#fff">CONTAINER</Heading>
-        <Flex direction="row" justifyContent="space-around">
+        <Heading color="#fff" m={4}>CONTAINER APP</Heading>
+
+        <Flex direction="row" justifyContent="space-between" width="80vw">
           <React.Suspense fallback={<Spinner size="xl" />}>
             <Box
               p="2rem"
@@ -56,6 +54,7 @@ const App = () => (
               <CounterAppOne />
             </Box>
           </React.Suspense>
+          <Spacer />
           <React.Suspense fallback={<Spinner size="xl" />}>
             <Box
               p="2rem"
@@ -66,19 +65,25 @@ const App = () => (
               <Heading color="#6F60EA" mb="1rem">
                 APP-2
               </Heading>
-              {/* <CounterAppTwo /> */}
+              <CounterAppTwo />
+            </Box>
+          </React.Suspense>
+          <Spacer />
+          <React.Suspense fallback={<Spinner size="xl" />}>
+            <Box
+              p="2rem"
+              border="1px solid #aeaeae"
+              borderRadius="1rem"
+              backgroundColor="#fff"
+            >
+              <Heading color="#6F60EA" mb="1rem">
+                APP-3
+              </Heading>
               <LeftSideBar />
             </Box>
           </React.Suspense>
         </Flex>
       </Flex>
-      <Link
-        marginTop="5rem"
-        href="https://github.com/ogzhanolguncu/react-typescript-module-federation"
-        target="_blank"
-      >
-        <Image src="./git.png" height="45px" width="45px" />
-      </Link>
     </Center>
   </>
 );
